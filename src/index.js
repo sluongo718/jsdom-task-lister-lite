69baedc0f11abd81187b455b36984de08911e4b8
sluongo718 <sluongo718@yahoo.com>
2021-06-25 02:05:16 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('form');
+  const input = document.querySelector('input');
+  const task = document.querySelector('#tasks');
+
+    form.addEventListener('submit', (event) => {
+      const li = document.createElement('li');
+      const button = document.createElement('button')
+      const value = input.value
+
+      li.innerText = value
+      button.innerText = 'x'
+      button.id = value
+      li.appendChild(button)
+      task.appendChild(li)
+      input.value = ''
+      event.preventDefault();
+
+      button.addEventListener('click', () => {
+        li.remove()
+      });
+
+    });
 });
